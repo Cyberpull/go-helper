3be91513b6906f022fe21135f0146341df89cbd2
Christian Ezeani <christian.ezeani@gmail.com>
2023-06-15 03:40:40 +0100

    Refactor `validator` package

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/fatih/color v1.15.0
+	github.com/go-playground/validator/v10 v10.14.1
 	github.com/google/uuid v1.3.0
 	github.com/joho/godotenv v1.5.1
 	github.com/pquerna/otp v1.4.0
@@ -15,11 +16,18 @@ require (
 require (
 	github.com/boombuler/barcode v1.0.1-0.20190219062509-6c824513bacc // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/crypto v0.7.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- validator/default.go
+++ validator/default.go
@@ -3,84 +3,42 @@ package validator
 import (
 	"reflect"
 
-	"cyberpull.com/gotk/errors"
+	"github.com/go-playground/validator/v10"
 )
 
-func Validate(data any, info ...*Validation) (err error) {
-	rValue := reflect.ValueOf(data)
-	rType := rValue.Type()
+var validate *validator.Validate
 
-	if rType.Kind() == reflect.Pointer {
+func Validate(data any, rules ...any) (err error) {
+	rValue := reflect.ValueOf(data)
+
+	rType := rValue.Type()
+	rKind := rType.Kind()
+
+	if rKind == reflect.Pointer {
 		rValue = rValue.Elem()
 	}
 
-	rKind := rType.Kind()
-
-	if rKind == reflect.Struct {
-		err = validateStruct(rValue)
-		return
-	}
-
-	if len(info) == 0 {
-		info = append(info, &Validation{})
-	}
+	value := rValue.Interface()
 
 	switch rKind {
-	case reflect.String:
-		err = validateString(rValue.String(), info[0])
-	case reflect.Bool:
-		err = validateBool(rValue.Bool(), info[0])
+	case reflect.Struct:
+		return validate.Struct(value)
+	default:
+		rule := one[string]("", rules)
+		return validate.Var(value, rule)
 	}
-
-	return
 }
 
-func validateStruct(data reflect.Value) (err error) {
-	length := data.NumField()
-
-	for i := 0; i < length; i++ {
-		field := data.Field(i)
-		rType := data.Type().Field(i)
-
-		tag, ok := rType.Tag.Lookup("validator")
-
-		if !ok || tag == "" {
-			continue
-		}
-
-		info := parseValidationTag(rType, tag)
-
-		if err = Validate(field.Interface(), info); err != nil {
-			break
-		}
+func one[T any](def T, attr []any) T {
+	if len(attr) == 0 {
+		return attr[0].(T)
 	}
 
-	return
+	return def
 }
 
-func validateString(data string, info *Validation) (err error) {
-	if info.Options.Required.Value && data == "" {
-		message := message(Required, info)
-		err = errors.New(message, 400)
-		return
-	}
+// ==========================
 
-	if info.Options.Email.Value {
-		err = checkEmail(data, info)
-
-		if err != nil {
-			return
-		}
-	}
-
-	return
-}
-
-func validateBool(data bool, info *Validation) (err error) {
-	if info.Options.Required.Value && !data {
-		message := message(Required, info)
-		err = errors.New(message, 400)
-	}
-
-	return
+func init() {
+	validate = validator.New()
 }
