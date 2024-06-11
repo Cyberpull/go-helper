3c41e3863f91f3d3c659466fe25284b57f570ecb
Christian Ezeani <christian.ezeani@gmail.com>
2024-06-11 11:24:52 +0100

    Fix pagination bug

--- dbo/pagination.go
+++ dbo/pagination.go
@@ -10,13 +10,13 @@ import (
 )
 
 type Pagination[T any] struct {
-	Current_page uint `json:"current_page"`
-	From         uint `json:"from"`
-	Last_page    uint `json:"last_page"`
-	Per_page     int  `json:"per_page"`
-	To           uint `json:"to"`
-	Total        uint `json:"total"`
-	Data         []T  `json:"data"`
+	CurrentPage uint `json:"current_page"`
+	From        uint `json:"from"`
+	LastPage    uint `json:"last_page"`
+	PerPage     int  `json:"per_page"`
+	To          uint `json:"to"`
+	Total       uint `json:"total"`
+	Data        []T  `json:"data"`
 }
 
 func Paginate[T any](tx *gorm.DB, page uint, limit ...uint) (value *Pagination[T], err error) {
@@ -28,39 +28,44 @@ func Paginate[T any](tx *gorm.DB, page uint, limit ...uint) (value *Pagination[T
 		return
 	}
 
-	tx = tx.Offset(0).Limit(-1).Session(&gorm.Session{})
+	tx = tx.Offset(-1).Limit(-1).Session(&gorm.Session{})
 
 	tmpValue := &Pagination[T]{}
 	tmpValue.Data = make([]T, 0)
-	tmpValue.Current_page = uint(math.Max(float64(page), 1))
-	tmpValue.Per_page = int(math.Max(float64(limit[0]), 1))
+	tmpValue.CurrentPage = uint(math.Max(float64(page), 1))
+	tmpValue.PerPage = int(math.Max(float64(limit[0]), 1))
 
-	if tmpValue.Current_page == 1 {
-		tmpValue.From = tmpValue.Current_page
-	} else {
-		tmpValue.From = tmpValue.Current_page * uint(tmpValue.Per_page)
+	offset := int(0)
+
+	if tmpValue.CurrentPage > 1 {
+		offset = int(tmpValue.CurrentPage) - 1
+		offset *= tmpValue.PerPage
 	}
 
-	offset := int(tmpValue.From) - 1
+	if offset > 0 {
+		tmpValue.From = uint(offset) + 1
+	} else {
+		tmpValue.From = 1
+	}
 
-	tx = tx.Offset(offset).Limit(tmpValue.Per_page)
+	tx = tx.Offset(offset).Limit(tmpValue.PerPage)
 
 	if err = tx.Find(&tmpValue.Data).Error; err != nil {
 		return
 	}
 
-	tmpValue.To = tmpValue.From + uint(len(tmpValue.Data))
+	tmpValue.To = uint(offset) + uint(len(tmpValue.Data))
 
 	var total int64
 
-	tx = tx.Offset(0).Limit(-1)
+	tx = tx.Offset(-1).Limit(-1)
 
 	if err = tx.Count(&total).Error; err != nil {
 		return
 	}
 
-	lastPage := float64(total) / float64(len(tmpValue.Data))
-	tmpValue.Last_page = uint(math.Ceil(lastPage))
+	lastPage := float64(total) / float64(tmpValue.PerPage)
+	tmpValue.LastPage = uint(math.Ceil(lastPage))
 	tmpValue.Total = uint(total)
 
 	value = tmpValue
